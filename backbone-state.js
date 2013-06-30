63b951b32a2a20dd2917646e941551cab4d7ea10
Rajiv <rajivtreddy@gmail.com>
2013-06-29 18:15:15 -0700

    Resolved #2

--- backbone-state.js
+++ backbone-state.js
@@ -24,40 +24,44 @@
       };
     }
 
-    // Setup `this.state` to store state values, and grab apply any initial
+    // Setup `this._state` to store state values, and grab apply any initial
     // state values passed in as options.
     this.prototype._configureStates = function (states, options) {
       if (_.isFunction(options)) options = options();
-      this.state = {};
+      this._state = {};
       for (var i = 0, state; state = states[i]; i++) {
-        this.state[state] = options[state] === true;
+        this._state[state] = options[state] === true;
       }
     };
 
 
-    // Getters + Setters
+    // Combined: Getters + Setters
     // ------
 
+    // syntax: state(state)
     // Retrive the view's current state.
-    this.prototype.getState = function (state) {
-      return this.state[state];
-    };
 
+    // syntax: state(state, value)
     // Sets a view's state and tries to update its `$el`'s classes/attributes
     // with the jQuery-State plugin if it's available. Triggers two events, so
     // that you can either listen for `change:state` and receive the state's
     // name and value or for `change:state:hidden` and just receive its value.
-    this.prototype.setState = function (state, value) {
-      if (this.state[state] === value) return this;
-      this.state[state] = value;
-      if (this.$el && this.$el.state) this.$el.state(state, value);
-      this.trigger('change:state:'+state, this, value);
-      this.trigger('change:state', this, state, value);
+    this.prototype.state = function (state, value) {
+      if(arguments.length > 1){
+        if (this._state[state] === value) return this;
+        this._state[state] = value;
+        if (this.$el && this.$el.state) this.$el.state(state, value);
+        this.trigger('change:state:'+state, this, value);
+        this.trigger('change:state', this, state, value);
+      } else {
+        return this._state[state];
+      }
+    
     };
 
     // Shortcut for toggling a view's state.
     this.prototype.toggleState = function (state, toggle) {
-      return this.setState(state, !this.getState(state));
+      return this.state(state, !this.state(state));
     };
 
 
@@ -67,7 +71,7 @@
     // Bind to whenever an view's state changes to a particular boolean value.
     this.prototype.onState = function (state, value, callback, context) {
       // Callback right away if its in the right state already.
-      var current = this.getState(state);
+      var current = this.state(state);
       if (current === value) callback.call(context, this, current);
 
       this.on('change:state:'+state, function (view, stateValue) {
@@ -79,7 +83,7 @@
     // Same as `onState`, but only ever fires the callback once.
     this.prototype.onceState = function (state, value, callback, context) {
       // Callback right away if its in the right state already.
-      var current = this.getState(state);
+      var current = this.state(state);
       if (current === value) {
         callback.call(context, this, current);
         return this;
